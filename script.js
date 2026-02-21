5f15e2f81fafc8f9a7b71b3e1d9b2a5972e2911f
kelsey-kelz <183586891+kelsey-kelz@users.noreply.github.com>
2026-02-21 12:33:08 +0300

    Edit file

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let h1 = document.querySelector("h1");
+const myLibrary = [];//array to store the books
 
-h1.setAttribute("style", "color:blue; background:white;");
-
-
-const myLibrary = [];
-
+//a book constructor
 function Book(title, author, pages, readStatus) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -20,13 +16,12 @@ function Book(title, author, pages, readStatus) {
     }
 }
 
-// const book1 = new Book("The Hobbit", "J.R.R Tolkien", "295", "not read yet");
-// console.log(book1.info());
-
+//take params, create a book then store it in the array
 function addBookToLibrary(title, author, pages, readStatus) {
     const newBook = new Book(title, author, pages, readStatus);
     newBook.id = crypto.randomUUID();
     myLibrary.push(newBook);
+    displayBook(newBook);
     return newBook;
 }
 
@@ -39,3 +34,16 @@ console.log(myLibrary);
 const book3 = addBookToLibrary("The What is this", "J.R.R Tolkien", "295", "not read yet");
 console.log(book3);
 console.log(myLibrary);
+
+function displayBook(book) {
+const div = document.querySelector("div");
+
+const bookElement = document.createElement("div");
+bookElement.innerHTML = `
+    <h2>${book.title}</h2>
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Status: ${book.readStatus}</p>
+    `;
+    div.appendChild(bookElement);
+}    
